3d23665d72a5a7576974de2892e28d432822c884
Nicklas Schioldan Winther <nick3845@edu.campusvejle.dk>
2019-04-11 11:41:16 +0200

    Added images to rides

--- RideServiceGroup2.DAL/DatabaseCreation.sql
+++ RideServiceGroup2.DAL/DatabaseCreation.sql
@@ -38,11 +38,11 @@ INSERT INTO RideCategories
 VALUES('Rutsjebane', 'Forlystelser som består af en bane med stejle fald, hvorår man kører i små vogne')
 
 INSERT INTO Rides
-VALUES('Orkanen', '/img1.png', 'Orkanen er en fantastisk og hæsblæsende rutsjebane for hele familien. Den har alt hvad adrenalinhungrende fartdjævle med hang til store højder og dybe fald tør drømme om!','Orkanen giver dig en tur, du aldrig glemmer', '3', '1')
+VALUES('Orkanen', '/orkanen.jpg', 'Orkanen er en fantastisk og hæsblæsende rutsjebane for hele familien. Den har alt hvad adrenalinhungrende fartdjævle med hang til store højder og dybe fald tør drømme om!','Orkanen giver dig en tur, du aldrig glemmer', '3', '1')
 INSERT INTO Rides
-VALUES('Vandcyklonen', '/img2.png', 'Vandcyklonen er en rutsjebane, der med garanti gør dig plaskhamrende rundtosset. Spring om bord i den store 2-personers badering og hold godt fast, når det går susende rundt i en kæmpemæssig tragt, der pludselig ender brat i en… aaaaargh… stejl og mørk tunnel!', 'Spring om bord i en stor 2-personers badering','1', '1')
+VALUES('Vandcyklonen', '/vandcyklonen.jpg', 'Vandcyklonen er en rutsjebane, der med garanti gør dig plaskhamrende rundtosset. Spring om bord i den store 2-personers badering og hold godt fast, når det går susende rundt i en kæmpemæssig tragt, der pludselig ender brat i en… aaaaargh… stejl og mørk tunnel!', 'Spring om bord i en stor 2-personers badering','1', '1')
 INSERT INTO Rides
-VALUES('Snurretræet', '/img3.png', 'Træstammen vugger først stille og roligt frem og tilbage, men pludselig begynder den også at snurre rundt om sig selv - og så er det bare om at holde godt fast. Aaargh!', 'Kæmpe træstamme, der snurrer rundt om sig selv', '2', '1')
+VALUES('Snurretræet', '/snurretraeet.jpg', 'Træstammen vugger først stille og roligt frem og tilbage, men pludselig begynder den også at snurre rundt om sig selv - og så er det bare om at holde godt fast. Aaargh!', 'Kæmpe træstamme, der snurrer rundt om sig selv', '2', '1')
 
 INSERT INTO Reports
 VALUES('3', '2018-07-26', 'Virker ikke', '1');

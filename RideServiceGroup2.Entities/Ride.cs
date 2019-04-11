548f0d168ae874f56d4d76309035cf42b8379076
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-04-11 10:38:46 +0200

    Ride class updated

--- RideServiceGroup2.Entities/Ride.cs
+++ RideServiceGroup2.Entities/Ride.cs
@@ -7,6 +7,7 @@ namespace RideServiceGroup2.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ImgUrl { get; set; }
         public string Description { get; set; }
         public RideCategory Category { get; set; }
         public Status Status { get; }

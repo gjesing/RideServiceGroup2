1f5e5c395696158a4861428ee13a5fa15cb720f9
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-01 15:03:34 +0200

    Created unit tests for the method NumberOfShutDowns() in the ride class

--- RideServiceGroup2.Entities.Test/RideTest.cs
+++ RideServiceGroup2.Entities.Test/RideTest.cs
@@ -42,5 +42,41 @@ namespace RideServiceGroup2.EntitiesTest
             int actualValue = ride.DaysSinceLastShutdown();
             Assert.Equal(expectedValue, actualValue);
         }
+
+        [Fact]
+        public void NumberOfShutDowns_Return0WithNoReports()
+        {
+            Ride ride = new Ride();
+            int expectedValue = 0;
+            int actualValue = ride.NumberOfShutdowns();
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void NumberOfShutDowns_Return0WithNoBrokenReports()
+        {
+            Ride ride = new Ride() { Reports = new List<Report>() { new Report() } };
+            int expectedValue = 0;
+            int actualValue = ride.NumberOfShutdowns();
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void NumberOfShutDowns_Return1WithBrokenReport()
+        {
+            Ride ride = new Ride() { Reports = new List<Report>() { new Report() { Status = Status.Broken } } };
+            int expectedValue = 1;
+            int actualValue = ride.NumberOfShutdowns();
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void NumberOfShutDowns_ReturnCorrectWithMultipleBrokenReports()
+        {
+            Ride ride = new Ride() { Reports = new List<Report>() { new Report() { Status = Status.Broken }, new Report() { Status = Status.Broken } } };
+            int ExpectedValue = 2;
+            int actualValue = ride.NumberOfShutdowns();
+            Assert.Equal(ExpectedValue, actualValue);
+        }
     }
 }
